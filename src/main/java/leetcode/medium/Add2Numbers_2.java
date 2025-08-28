6331fa933408cc9d57ea4e930978118e81b4158d
Pankaj Sao <67387430+voiceofpanky@users.noreply.github.com>
2025-08-28 11:08:09 +0000

    updated leetcode problems

---
+++ src/main/java/leetcode/medium/Add2Numbers_2.java
@@ -0,0 +1,63 @@
+package leetcode.medium;
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+
+            current = current.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return dummy.next;
+    }
+}
+
+public class Add2Numbers_2 {
+    public static void main(String[] args) {
+        // Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        Solution sol = new Solution();
+        ListNode result = sol.addTwoNumbers(l1, l2);
+
+        // Print result in [7, 0, 8] format
+        System.out.println(listToString(result));
+    }
+
+    private static String listToString(ListNode node) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        while (node != null) {
+            sb.append(node.val);
+            node = node.next;
+            if (node != null) sb.append(", ");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
